aae66ae9883a5b1031e4e2bf4fa9e8e7f98205e3
z0z0r4 <z0z0r4@outlook.com>
2025-12-25 01:20:52 +0800

    Site updated: 2025-12-25 01:20:52

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766571520758";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766596849991";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
