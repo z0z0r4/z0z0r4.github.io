8f455c22dfe0181177f18a229726754c017e7433
z0z0r4 <z0z0r4@outlook.com>
2026-01-01 16:49:26 +0800

    Site updated: 2026-01-01 16:49:26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1767110950504";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1767257363775";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
